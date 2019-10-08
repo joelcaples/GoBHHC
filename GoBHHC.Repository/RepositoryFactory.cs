31605671f9459cbb2d4768ad5f5a4a8ffa038c06
Joel Caples <jmcaples@gmail.com>
2019-10-08 08:45:22 -0500

    Added configuration that will share the database file in the Repository project

--- GoBHHC.Repository/RepositoryFactory.cs
+++ GoBHHC.Repository/RepositoryFactory.cs
@@ -1,20 +1,31 @@
-﻿using System.Data.SQLite;
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using System.Data.SQLite;
 using System.IO;
 
 namespace GoBHHC.Repository {
-    public static class RepositoryFactory {
 
-        private const string DB_PATH = "database.db";
+    public static class RepositoryFactory {
 
         public static IListMgrRepository GetRepository() {
 
             var doCreate = false;
 
-            if (!File.Exists(DB_PATH))
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var csBuilder = new DbConnectionStringBuilder();
+            csBuilder.ConnectionString = configuration.GetConnectionString("Prodution");
+
+            if (!File.Exists((string)csBuilder["Data Source"]))
                 doCreate = true;
 
+
             // Create a new database connection:
-            var connection = new SQLiteConnection(@"Data Source=" + DB_PATH + ";Version=3;Compress=True;");
+            var connection = new SQLiteConnection(csBuilder.ConnectionString);
 
             // Open the connection:
             connection.Open();
